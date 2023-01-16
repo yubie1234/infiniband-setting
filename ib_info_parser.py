a4e285d7916b5355132a0f82a5c94698beb5df67
root <yeobie@acryl.ai>
2023-01-16 16:00:28 +0000

    mst, vf 설정관련 추가

--- ib_info_parser.py
+++ ib_info_parser.py
@@ -15,19 +15,33 @@ class AllInfiniband():
 
     def get_infiniband_devices_path(self):
         path_list = subprocess.check_output('find /sys/devices -name infiniband | sort', shell=True, stderr=subprocess.STDOUT).decode("utf-8").split()
+        """
+            /sys/devices/pci0000:16/0000:16:02.0/0000:17:00.0/infiniband
+            /sys/devices/pci0000:16/0000:16:02.0/0000:17:00.1/infiniband
+            /sys/devices/pci0000:16/0000:16:02.0/0000:17:00.2/infiniband
+        """
         for i, path in enumerate(path_list): 
             path_list[i] = path.replace("infiniband", "")
 
         return path_list
 
+    def get_infiniband(self, interface_name):
+        for infiniband in self.infiniband_list:
+            if infiniband.interface_name == interface_name:
+                return infiniband
+
+
 class Infiniband:
 
     def __init__(self, path):
+        self.path = path
         self.interface_name = self.get_interface_name(path=path)
         self.mlx_name = self.get_mlx_name(path=path)
         self.sriov_func = self.get_sriov_func(path=path)
-        self.num_of_vf = self.get_num_of_vf(path=path)
+        self.numvfs = self.get_numvfs(path=path) if self.sriov_func == SRIOV_PF else 0
+        self.totalvfs = self.get_totalvfs(path=path) if self.sriov_func == SRIOV_PF else 0
 
+        self.node_guid = self.get_node_guid(path=path)
 
     def get_interface_name(self, path):
         interface_name = os.listdir(path+"/net/")[-1]
@@ -45,14 +59,26 @@ class Infiniband:
         else :
             return SRIOV_VF
 
-    def get_num_of_vf(self, path):
+    def get_numvfs(self, path):
+        # 실제 활성화 되어 있는 vf 개수 sriov_numvfs
         # PF - 0 ~ N
         # VF - 0
-        if os.path.isdir(path+"/sriov/"):
-            return len(os.listdir(path+"/sriov/"))
-        else :
-            return 0
+        item = "/sriov_numvfs"
+        with open(path + item, "r") as f:
+            numvfs = f.read().replace("\n", "")
 
+        return numvfs
 
+    def get_totalvfs(self, path):
+        # 활성화 할 수 있는 총 vf 개수 sriov_totalvfs
+        # + /sriov_totalvfs
+        item = "/sriov_totalvfs"
+        with open(path + item, "r") as f:
+            totalvfs = f.read().replace("\n", "")
+        return totalvfs
 
-
+    def get_node_guid(self, path):
+        item = "/infiniband/{mlx_name}/node_guid".format(mlx_name=self.mlx_name)
+        with open(path + item, "r") as f:
+            node_guid = f.read().replace("\n", "")
+        return node_guid
---
+++ mst_info_parser.py
@@ -0,0 +1,157 @@
+#!/bin/bash
+"""
+mst start
+
+mst status
+
+MST_DEVICE=$(mst status | grep /dev | awk '{print $1}')
+
+MST_SRIOV_EN=$(mlxconfig -d $MST_DEVICE q | grep SRIOV_EN | awk '{print $2}') # True(1) or False(0)
+
+MST_NUM_OF_VFS=$(mlxconfig -d $MST_DEVICE q | grep NUM_OF_VFS | awk '{print $2}')
+
+NUM_OF_GPU=$(nvidia-smi -q -x | grep "gpu id" | wc -l)
+
+NEED_NUM_OF_VFS=$(($NUM_OF_GPU + 1))
+
+if [ "$MST_SRIOV_EN" == "True(1)" ] && [ "$MST_NUM_OF_VFS" == "$NEED_NUM_OF_VFS" ]
+then
+    echo "SKIP SRIOV_EN NUM_OF_VFS SET "
+    echo "RUN mst_after.sh"
+else
+    mlxconfig -d $MST_DEVICE set SRIOV_EN=1 NUM_OF_VFS=$NEED_NUM_OF_VFS
+fi
+"""
+import subprocess
+class BinaryNotExistError(Exception):
+    pass
+
+class BinaryNotWorkingError(Exception):
+    pass
+ 
+
+MST_TRUE = "True(1)"
+MST_FALSE = "False(0)"
+
+class Mst:
+    def __init__(self):
+        self._system_check() # binary exist check
+
+        self.mst_device = self.get_mst_device()
+        self.mst_query = self.get_mst_query(mst_device=self.mst_device)
+
+        # Query 관련
+        self.mst_sriov_en = self.get_mst_sriov_en()
+        self.mst_numvfs = self.get_mst_numvfs()
+
+    def _system_check(self):
+        try:
+            subprocess.check_output(["which mst && which mlxconfig"], shell=True, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as se:
+            raise BinaryNotExistError("[mst | mlxconfig] not exist. check ofed installed.")
+        # If mst or mlxconfig not 
+
+    def get_mst_device(self):
+        try:
+            # mst status | grep /dev | awk '{print $1}'
+            mst_status = subprocess.check_output(["mst status "], shell=True, stderr=subprocess.STDOUT).decode("utf-8").split()
+            for line in mst_status:
+                if "/dev/mst" in line:
+                    return line
+        except subprocess.CalledProcessError as scpe:
+            raise BinaryNotWorkingError("mst not working. \n returncode [{}]. \n output [{}]".format(scpe.returncode, scpe.stdout.decode("utf-8")))
+        
+        raise Exception("mst device not found.")
+
+    def get_mst_query(self, mst_device):
+        try:
+            mst_query = subprocess.check_output(["mlxconfig -d {MST_DEVICE} q".format(MST_DEVICE=mst_device)], shell=True, stderr=subprocess.STDOUT).decode("utf-8").split("\n")
+        except subprocess.CalledProcessError as scpe:
+            raise BinaryNotWorkingError("mlxconfig not working. \n returncode [{}]. \n output [{}]".format(scpe.returncode, scpe.stdout.decode("utf-8")))
+        return mst_query
+
+    def get_mst_value_by_key(self, key):
+        for query in self.mst_query:
+            if " {key} ".format(key=key) in query:
+                return query.split()[-1]
+
+        raise Exception("{key} KEY Not Found.".format(key))
+
+    def get_mst_sriov_en(self):
+        return self.get_mst_value_by_key(key="SRIOV_EN")
+
+    def get_mst_sriov_en_set_command(self, sriov_en: bool):
+        command = " SRIOV_EN={} ".format(str(sriov_en))
+        return command
+
+        # sriov_en = 1 if sriov_en == True else 0
+        # sriov_en_setting = subprocess.check_output(["echo y | mlxconfig -d {MST_DEVICE} set SRIOV_EN={SRIOV_EN}".format(MST_DEVICE=self.mst_device, SRIOV_EN=sriov_en)], shell=True, stderr=subprocess.STDOUT).decode("utf-8")
+        # print(sriov_en_setting)
+
+
+    def get_mst_numvfs(self):
+        # sriov_numvfs = subprocess.check_output(["mlxconfig -d {MST_DEVICE} q | grep NUM_OF_VFS | awk '{{print $2}}'".format(MST_DEVICE=mst_device)], shell=True, stderr=subprocess.STDOUT).decode("utf-8").replace("\n","")
+        # return sriov_numvfs
+        return int(self.get_mst_value_by_key(key="NUM_OF_VFS"))
+
+    def get_mst_numvfs_set_command(self, numvfs: int):
+        command = " NUM_OF_VFS={} ".format(numvfs)
+        return command
+        
+        # numvfs_setting = subprocess.check_output(["echo y | mlxconfig -d {MST_DEVICE} set NUM_OF_VFS={NEED_NUM_OF_VFS}".format(MST_DEVICE=self.mst_device, NEED_NUM_OF_VFS=numvfs)], shell=True, stderr=subprocess.STDOUT).decode("utf-8")
+        # print(numvfs_setting)
+
+    def set_mst_for_sriov(self, sriov_en: bool=None, numvfs: int=None):
+
+        if sriov_en is None and numvfs is None:
+            raise Exception("Setting Value Empty.")
+
+        if self.is_same_config(sriov_en=sriov_en, numvfs=numvfs):
+            print("Same Setting Skip.")
+            return True
+
+        set_command = ""
+        if sriov_en is not None:
+            set_command += self.get_mst_sriov_en_set_command(sriov_en=sriov_en)
+
+        if numvfs is not None:
+            set_command += self.get_mst_numvfs_set_command(numvfs=numvfs)
+
+        
+        mst_setting = subprocess.check_output(["echo y | mlxconfig -d {MST_DEVICE} set {SET_COMMAND}".format(MST_DEVICE=self.mst_device, SET_COMMAND=set_command)], shell=True, stderr=subprocess.STDOUT).decode("utf-8")
+        print(mst_setting)
+
+    # 
+    def is_sriov_avaliable(self):
+        is_avaliable = True
+        if self.mst_sriov_en == MST_FALSE:
+            # SRIOV EN ?
+            print("[Warn] Need to set SRIOV_EN=1. Current SRIOV_EN={}".format(self.mst_sriov_en))
+            is_avaliable = False
+
+        if self.mst_numvfs == 0:
+            # NUMVFS more than 0 ?
+            print("[Warn] Need to set NEED_NUM_OF_VFS=(More than 1). Current NUM_OF_VFS={}".format(self.mst_numvfs))
+            is_avaliable = False
+        return is_avaliable
+
+
+    def is_same_config(self, sriov_en, numvfs):
+        if sriov_en is None:
+            sriov_en = self.mst_sriov_en
+        else:
+            sriov_en = MST_TRUE if sriov_en == True else MST_FALSE 
+
+        if numvfs is None:
+            numvfs = self.mst_numvfs
+
+        # print(sriov_en, self.mst_sriov_en, numvfs, self.mst_numvfs)
+
+        if sriov_en == self.mst_sriov_en and numvfs == self.mst_numvfs:
+            return True
+        else:
+            return False
+
+
+mst = Mst()
+print(mst.set_mst_for_sriov(sriov_en=True, numvfs=10))
